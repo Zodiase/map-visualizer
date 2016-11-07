f02828081ac21a79079d96153284bb59cebcd798
Xingchen Hong <hello@xc-h.net>
2016-11-07 16:58:49 -0600

    WIP.

--- src/app.js
+++ src/app.js
@@ -29,242 +29,244 @@ import {
   Viewer
 } from './viewer.js';
 
-const $mapContainer = $('#map'),
-      $notificationContainer = $('#notifications');
-if ($mapContainer.length === 0 || $notificationContainer.length === 0) {
-  throw new ReferenceError('Can not find elements.');
-}
+class App {
+  /**
+   * @param {string} options.mapContainerSelector
+   * @param {string} options.notificationContainerSelector
+   */
+  constructor ({mapContainerSelector, notificationContainerSelector}) {
+    this.$mapContainer_ = $(mapContainerSelector);
+    this.$notificationContainer_ = $(notificationContainerSelector);
+    if (this.$mapContainer_.length === 0 || this.$notificationContainer_.length === 0) {
+      throw new ReferenceError('Can not find elements.');
+    }
 
+    this.viewer_ = new Viewer({
+      target: this.$mapContainer_[0]
+    });
 
-const viewer = new Viewer({
-  target: $mapContainer[0]
-});
-
-//
-// // Start map loading.
-// const map = new ol.Map({
-//   target: $mapContainer[0],
-//   controls: ol.control.defaults().extend([
-//     layerListControl
-//   ]),
-//   view: null
-// });
-// const mainLayerGroup = map.getLayerGroup();
-// const mainLayerCollection = mainLayerGroup.getLayers();
-
-// Runtime data.
-let
+    // Runtime data.
     /**
      * If true, the app is busy processing a hash change.
      * @type {Boolean}
      */
-    busy = false,
+    this.busy_ = false;
     /**
      * If true, the app has successfully loaded a valid source file.
      * @type {Boolean}
      */
-    loaded = false,
+    this.loaded_ = false;
     /**
      * If loaded is true, this is the url of the loaded source file.
      * @type {String|null}
      */
-    loadedSourceUrl = null,
+    this.loadedSourceUrl_ = null;
     /**
      * If loaded is true, this is the data of the loaded source file.
      * @type {Object|null}
      */
-    loadedSourceData = null,
+    this.loadedSourceData_ = null;
     /**
      * Stores the ID of the timer that is used to update the extent config in the url.
      * @type {Number|null}
      */
-    extentUpdateTimer = null,
+    this.extentUpdateTimer_ = null;
     /**
      * Stores the last fitted stable map view extent.
      * @type {Array.<Number>|null}
      */
-    fitExtent = null;
+    this.fitExtent_ = null;
 
-/**
- * Start or restart the app with the given hash string.
- * @param {String} hash
- */
-const startWithHash = (hash) => {
-  if (busy) {
-    warn('Hash update while busy!');
-    location.reload();
-    return;
-  }
-  busy = true;
+    this.boundUserInteractionStart_ = this.userInteractionStart_.bind(this);
+    this.boundUserInteractionEnd_ = this.userInteractionEnd_.bind(this);
 
-  // Cancel any extent updates.
-  if (extentUpdateTimer !== null) {
-    window.clearTimeout(extentUpdateTimer);
-    extentUpdateTimer = null;
+    this.viewer_.on('change:center', this.boundUserInteractionStart_);
+    this.viewer_.on('change:resolution', this.boundUserInteractionStart_);
+    this.viewer_.on('map/change:size', this.userInteractionStart_);
+    this.viewer_.on('map/moveend', this.boundUserInteractionEnd_);
   }
 
-  info('Hash', hash);
-  const parse = parseHashString(hash);
-  info('parse', parse);
-  const sourceUrl = (parse.source || '').trim();
-  info('sourceUrl', sourceUrl);
-  const extra = {};
-  const configString = (parse.config || '').trim();
-  // Config String is optional.
-  extra.layerConfigs = parseLayerConfigString(configString);
-  info('extra.layerConfigs', extra.layerConfigs);
-  const extentString = (parse.extent || '').trim();
-  // Extent String is optional.
-  extra.extent = parseExtentString(extentString);
-  info('extra.extent', extra.extent);
-  if (loaded && sourceUrl === loadedSourceUrl) {
-    // Source Url didn't change.
-    log('Updating...');
-    // Update layers.
-    updateLayers(mainLayerCollection, extra.layerConfigs);
-    // Update map view extent.
-    const newExtent = (extra.extent !== null) ? extra.extent : loadedSourceData.extent;
-    if (!isIdenticalExtent(fitExtent, newExtent)) {
-      map.getView().fit(newExtent, map.getSize());
-      fitExtent = map.getView().calculateExtent(map.getSize());
-    }
-
-    layerListControl.update(extra.layerConfigs);
-
-    log('Updated');
-    busy = false;
-  } else {
-    log('Loading new...');
-    // Some resetting here.
-    loaded = false;
-    loadedSourceUrl = null;
-    loadedSourceData = null;
-    fitExtent = null;
-    mainLayerCollection.clear();
-    $notificationContainer.empty();
-    layerListControl.reload([], {});
-    setMapProjection(null);
-
-    $notificationContainer.append(
-      $('<div class="hashparse">')
-      .append($('<div>').text(`source: ${parse.source}`))
-      .append($('<div>').text(`config: ${JSON.stringify(extra.layerConfigs)}`))
-      .append($('<div>').text(`extent: ${JSON.stringify(extra.extent)}`))
-    );
-    // Source Url is necessary.
-    if (sourceUrl.length === 0) {
-      // No source url available.
-      warn('No source url available.');
-      $notificationContainer.append($('<span>').text('No source url available.'));
+  /**
+   * Start or restart the app with the given hash string.
+   * @param {String} hash
+   */
+  startWithHash (hash) {
+    if (this.busy_) {
+      warn('Hash update while busy!');
+      location.reload();
       return;
     }
-    log('Downloading source file...');
-    $notificationContainer.append($('<span>').text('Downloading source file...'));
-    $.getJSON(sourceUrl)
-    .fail((jqxhr, textStatus, err) => {
-      const errStr = `${textStatus}, ${err}`;
-      error(errStr);
-      $notificationContainer.append($('<span>').text(errStr));
-    })
-    .done((data) => {
-      info('Downloaded', data);
-      $notificationContainer.empty();
-      try {
-        // Load projection from source file or use default.
-        setMapProjection(data.projection || DefaultProjection);
-        // Load layers.
-        loadLayers(mainLayerCollection, data.layers);
-        // Update layers.
-        updateLayers(mainLayerCollection, extra.layerConfigs);
-        // Update map view extent.
-        const newExtent = (extra.extent !== null) ? extra.extent : data.extent;
+    this.busy_ = true;
+
+    // Cancel any extent updates.
+    if (this.extentUpdateTimer_ !== null) {
+      window.clearTimeout(this.extentUpdateTimer_);
+      this.extentUpdateTimer_ = null;
+    }
+
+    info('Hash', hash);
+    const parse = parseHashString(hash);
+    info('parse', parse);
+    const sourceUrl = (parse.source || '').trim();
+    info('sourceUrl', sourceUrl);
+    const extra = {};
+    const configString = (parse.config || '').trim();
+    // Config String is optional.
+    extra.layerConfigs = parseLayerConfigString(configString);
+    info('extra.layerConfigs', extra.layerConfigs);
+    const extentString = (parse.extent || '').trim();
+    // Extent String is optional.
+    extra.extent = parseExtentString(extentString);
+    info('extra.extent', extra.extent);
+    if (this.loaded_ && sourceUrl === this.loadedSourceUrl_) {
+      // Source Url didn't change.
+      log('Updating...');
+      // Update layers.
+      this.viewer_.updateLayers(mainLayerCollection, extra.layerConfigs);
+      // Update map view extent.
+      const newExtent = (extra.extent !== null) ? extra.extent : this.loadedSourceData_.extent;
+      if (!isIdenticalExtent(this.fitExtent_, newExtent)) {
         map.getView().fit(newExtent, map.getSize());
-        fitExtent = map.getView().calculateExtent(map.getSize());
-
-        layerListControl.reload(data.layers, extra.layerConfigs);
-
-        loaded = true;
-        loadedSourceUrl = sourceUrl;
-        loadedSourceData = data;
-        log('Loaded');
-      } catch (err) {
-        error(err);
-        $notificationContainer.append($('<span>').text(err));
+        this.fitExtent_ = map.getView().calculateExtent(map.getSize());
       }
-      busy = false;
+
+      layerListControl.update(extra.layerConfigs);
+
+      log('Updated');
+      this.busy_ = false;
+    } else {
+      log('Loading new...');
+      // Some resetting here.
+      this.loaded_ = false;
+      this.loadedSourceUrl_ = null;
+      this.loadedSourceData_ = null;
+      this.fitExtent_ = null;
+      mainLayerCollection.clear();
+      this.$notificationContainer_.empty();
+      layerListControl.reload([], {});
+      this.viewer_.setMapProjection(null);
+
+      this.$notificationContainer_.append(
+        $('<div class="hashparse">')
+        .append($('<div>').text(`source: ${parse.source}`))
+        .append($('<div>').text(`config: ${JSON.stringify(extra.layerConfigs)}`))
+        .append($('<div>').text(`extent: ${JSON.stringify(extra.extent)}`))
+      );
+      // Source Url is necessary.
+      if (sourceUrl.length === 0) {
+        // No source url available.
+        warn('No source url available.');
+        this.$notificationContainer_.append($('<span>').text('No source url available.'));
+        return;
+      }
+      log('Downloading source file...');
+      this.$notificationContainer_.append($('<span>').text('Downloading source file...'));
+      $.getJSON(sourceUrl)
+      .fail((jqxhr, textStatus, err) => {
+        const errStr = `${textStatus}, ${err}`;
+        error(errStr);
+        this.$notificationContainer_.append($('<span>').text(errStr));
+      })
+      .done((data) => {
+        info('Downloaded', data);
+        this.$notificationContainer_.empty();
+        try {
+          // Load projection from source file or use default.
+          this.viewer_.setMapProjection(data.projection || DefaultProjection);
+          // Load layers.
+          this.viewer_.loadLayers(mainLayerCollection, data.layers);
+          // Update layers.
+          this.viewer_.updateLayers(mainLayerCollection, extra.layerConfigs);
+          // Update map view extent.
+          const newExtent = (extra.extent !== null) ? extra.extent : data.extent;
+          map.getView().fit(newExtent, map.getSize());
+          this.fitExtent_ = map.getView().calculateExtent(map.getSize());
+
+          layerListControl.reload(data.layers, extra.layerConfigs);
+
+          this.loaded_ = true;
+          this.loadedSourceUrl_ = sourceUrl;
+          this.loadedSourceData_ = data;
+          log('Loaded');
+        } catch (err) {
+          error(err);
+          this.$notificationContainer_.append($('<span>').text(err));
+        }
+        this.busy_ = false;
+      });
+    }
+  }
+
+  /**
+   * Handler for the start of a user interaction on the map.
+   */
+  userInteractionStart_ () {
+    // Cancel pending extent updates.
+    if (this.extentUpdateTimer_ !== null) {
+      window.clearTimeout(this.extentUpdateTimer_);
+      this.extentUpdateTimer_ = null;
+    }
+  }
+
+  /**
+   * Handler for the end of a user interaction on the map.
+   */
+  userInteractionEnd_ () {
+    // If not loaded, ignore these events.
+    if (!this.loaded_) {
+      return;
+    }
+
+    // Cancel pending extent updates.
+    if (this.extentUpdateTimer_ !== null) {
+      window.clearTimeout(this.extentUpdateTimer_);
+      this.extentUpdateTimer_ = null;
+    }
+
+    const viewExtent = map.getView().calculateExtent(map.getSize());
+
+    // Check if need to update extent.
+    if (isIdenticalExtent(this.fitExtent_, viewExtent)) {
+      return;
+    }
+
+    this.fitExtent_ = viewExtent;
+
+    this.extentUpdateTimer_ = window.setTimeout(this.setHashViewExtent.bind(this, viewExtent), extentUpdateDelay);
+  }
+
+  /**
+   * Set the view extent in url hash.
+   * Updates the url hash when needed.
+   * @param {Array.<Number>} extent
+   */
+  setHashViewExtent (extent) {
+    this.extentUpdateTimer_ = null;
+
+    // If not loaded, do nothing.
+    if (!this.loaded_) {
+      return;
+    }
+
+    // Update Hash.
+    const extentString = buildExtentString(extent);
+    setHashValue({
+      "extent": extentString
     });
   }
-};
+}
 
-/**
- * Set the view extent in url hash.
- * Updates the url hash when needed.
- * @param {Array.<Number>} extent
- */
-const setHashViewExtent = (extent) => {
-  extentUpdateTimer = null;
-
-  // If not loaded, do nothing.
-  if (!loaded) {
-    return;
-  }
-
-  // Update Hash.
-  const extentString = buildExtentString(extent);
-  setHashValue({
-    "extent": extentString
-  });
-};
-
-/**
- * Handler for the start of a user interaction on the map.
- */
-const userInteractionStart = () => {
-  // Cancel pending extent updates.
-  if (extentUpdateTimer !== null) {
-    window.clearTimeout(extentUpdateTimer);
-    extentUpdateTimer = null;
-  }
-};
-
-/**
- * Handler for the end of a user interaction on the map.
- */
-const userInteractionEnd = () => {
-  // If not loaded, ignore these events.
-  if (!loaded) {
-    return;
-  }
-
-  // Cancel pending extent updates.
-  if (extentUpdateTimer !== null) {
-    window.clearTimeout(extentUpdateTimer);
-    extentUpdateTimer = null;
-  }
-
-  const viewExtent = map.getView().calculateExtent(map.getSize());
-
-  // Check if need to update extent.
-  if (isIdenticalExtent(fitExtent, viewExtent)) {
-    return;
-  }
-
-  fitExtent = viewExtent;
-
-  extentUpdateTimer = window.setTimeout(setHashViewExtent.bind(this, viewExtent), extentUpdateDelay);
-};
-
-viewer.on('change:center', userInteractionStart);
-viewer.on('change:resolution', userInteractionStart);
-viewer.on('map/change:size', userInteractionStart);
-viewer.on('map/moveend', userInteractionEnd);
+const app = new App({
+  mapContainerSelector: '#map',
+  notificationContainerSelector: '#notifications'
+});
 
 $(window).on('load', () => {
-  startWithHash(location.hash);
+  app.startWithHash(location.hash);
   $(window).on('hashchange', () => {
     // Need to check if Source Url has been changed.
-    startWithHash(location.hash);
+    app.startWithHash(location.hash);
   });
 });
 
-window.__viewer = viewer;
+window.__app = app;
