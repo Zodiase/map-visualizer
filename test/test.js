0da308a96b04d0785ba57f213b2545e5cb8fb165
Xingchen Hong <hello@xc-h.net>
2017-01-12 16:14:48 -0600

    Fixed test for verifying that user should not be able to promote the top layer or demote the bottom layer.

--- test/test.js
+++ test/test.js
@@ -229,29 +229,37 @@ describe('source loading', function(){
                 browser.click(layerListToggleButtonQuery);
             });
 
-            it.skip('should not change layer order when trying to move the top layer up or bottom layer down', function(){
+            it('should not change layer order when trying to move the top layer up or bottom layer down', function(){
                 browser.reseturl("/#source=https://raw.githubusercontent.com/Zodiase/map-visualizer/gh-pages/sample-source/two-layers.json");
-                browser.waitForExist('.layer-list__toggle button');
-                var button = '.layer-list__toggle button';
-                browser.click(button);
+
+                // Wait for layer list to be ready.
+                var layerListToggleButtonQuery = '.layer-list__toggle button';
+                browser.waitForExist(layerListToggleButtonQuery);
+
+                // Expand the layer list.
+                browser.click(layerListToggleButtonQuery);
+                browser.pause(1000);
+
                 var row1 = '.layer-list__item:nth-child(1) .layer-list__item-row';
-                var row1_up = row1 + ' .layer-list__item__action-promote';
                 browser.waitForExist(row1);
+
+                var row1_up = row1 + ' .layer-list__item__action-promote';
                 var original_label = browser.getAttribute('.layer-list__item:nth-child(1)', 'data-layer-id');
-                browser.moveToObject(row1);
-                browser.waitForVisible(row1_up, 5000);
-                browser.click(row1_up);
+                browser.moveToObject(row1).moveToObject(row1_up).click(row1_up);
                 expect(browser.getAttribute('.layer-list__item:nth-child(1)', 'data-layer-id')).to.equal(original_label);
 
                 var row2 = '.layer-list__item:nth-child(2) .layer-list__item-row';
-                var original_label2 = browser.getAttribute('.layer-list__item:nth-child(2)', 'data-layer-id');
+                browser.waitForExist(row1);
+
                 var row2_down = row2 + ' .layer-list__item__action-demote';
-                browser.moveToObject(row2);
-                browser.waitForVisible(row2_down, 5000);
-                browser.click(row2_down);
+                var original_label2 = browser.getAttribute('.layer-list__item:nth-child(2)', 'data-layer-id');
+                browser.moveToObject(row2).moveToObject(row2_down).click(row2_down);
                 expect(browser.getAttribute('.layer-list__item:nth-child(2)', 'data-layer-id')).to.equal(original_label2);
-                browser.click(button);
+
+                // Collapse the layer list.
+                browser.click(layerListToggleButtonQuery);
             });
+
             it.skip('should see the correct layer order when config string include the order', function(){
                 browser.reseturl("/#source=https://raw.githubusercontent.com/Zodiase/map-visualizer/gh-pages/sample-source/two-layers.json");
                 browser.waitForExist('.layer-list__item');
